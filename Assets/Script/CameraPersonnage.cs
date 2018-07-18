f8dad93cc4fedd8ab440e388174fae34fa2bd496
LeilaniBex <axelle.bini02@gmail.com>
2018-07-18 14:09:48 +0200

    Deplacement dans l'espace camera

--- Assets/Script/CameraPersonnage.cs
+++ Assets/Script/CameraPersonnage.cs
@@ -3,33 +3,85 @@ using UnityEngine;
 
 public class CameraPersonnage : MonoBehaviour
 {
-    #region  Variables (public)
+	#region  Variables (public)
 
-    public PersonnageJoueur m_pTarget = null;
+	static public CameraPersonnage Instance = null;
 
-    public float m_fDistanceDeSuivi = 0.0f;
+	public PersonnageJoueur m_pTarget = null;
 
+	public Transform m_pCameraTransform = null;
 
-    #endregion
+	public float m_fDistanceDeSuivi = 0.0f;
 
-
-#region Variables (private)
-    
-   
+	public float m_fVitesseDeRotation = 0.0f;
 
 
-    #endregion
-    
-    private void LateUpdate()
-    {
-        if (m_pTarget != null)
-        SuivrePersonnage();
-    }
-    
-    private void SuivrePersonnage()
-    {
-        Vector3 tNouvellePosition = m_pTarget.transform.position + Vector3.up - (transform.forward * m_fDistanceDeSuivi);
-        transform.position = tNouvellePosition;
+	#endregion
 
-    }
+
+	#region Variables (private)
+
+
+
+
+	#endregion
+
+	private void Awake()
+	{
+		if (CameraPersonnage.Instance != null)
+		{
+			if (CameraPersonnage.Instance != this)
+				Destroy(this);
+
+			Debug.LogError("Attention, deux CameraPersonnage dans la scene");
+
+			return;
+		}
+
+		CameraPersonnage.Instance = this;
+	}
+
+	private void Update()
+	{
+		TournerCamera();
+	}
+
+	private void LateUpdate()
+	{
+		if (m_pTarget != null)
+		SuivrePersonnage();
+	
+	}
+	
+	private void SuivrePersonnage()
+	{
+		Vector3 tNouvellePositionPoint = m_pTarget.transform.position + Vector3.up;
+		transform.position = tNouvellePositionPoint;
+
+		Vector3 tNouvellePositionCamera = tNouvellePositionPoint - (m_pCameraTransform.forward * m_fDistanceDeSuivi);
+		m_pCameraTransform.position = tNouvellePositionCamera;
+
+	}
+
+	private void TournerCamera()
+	{
+		float fMouseX = Input.GetAxis("Mouse X");
+
+		Vector3 tRotationX = new Vector3(0.0f, fMouseX, 0.0f) * (m_fVitesseDeRotation * Time.deltaTime);
+
+		if (tRotationX != Vector3.zero)
+		{
+			transform.eulerAngles += tRotationX;
+		}
+
+		float fMouseY = Input.GetAxis("Mouse Y");
+
+		Vector3 tRotationY = new Vector3(fMouseY, 0.0f, 0.0f) * (m_fVitesseDeRotation * Time.deltaTime);
+
+		///if (tRotationY != Vector3.zero)
+		///{
+		///transform.eulerAngles += tRotationY;}
+
+		
+	}
 }
--- Assets/Script/PersonnageJoueur.cs
+++ Assets/Script/PersonnageJoueur.cs
@@ -3,61 +3,69 @@
 
 public class PersonnageJoueur : MonoBehaviour
 {
-    #region Variables (public)
+	#region Variables (public)
+	
 
-    public Rigidbody m_pRigidBody = null;
+	public Rigidbody m_pRigidBody = null;
 
-    public int m_iPv = 20;
+	public int m_iPv = 20;
 
-    public float m_fVitesse = 5.0f;
-    public float m_fVitesseDeSaut = 0.0f;
-    
-    #endregion
+	public float m_fVitesse = 5.0f;
+	public float m_fVitesseDeSaut = 0.0f;
+	
+	#endregion
 
-    #region Variables (private)
-
-
-
-
-    #endregion
+	#region Variables (private)
 
 
-    private void Start()
-    {
-    }
-
-    private void Update()
-    {
-        MoveCharacter();
-        Jump();
-    }
-
-    private void MoveCharacter()
-    {
-        float fHorizontal = Input.GetAxis("Horizontal");
-        float fVertical = Input.GetAxis("Vertical");
-
-        Vector3 tDirection = new Vector3(fHorizontal, 0.0f, fVertical);
-
-        // Pression sur espace --> faire le reste de la fct° //
-        if (tDirection != Vector3.zero)
-        {
-            tDirection.Normalize();
-
-            Vector3 tDeplacement = tDirection * (m_fVitesse * Time.deltaTime);
-            m_pRigidBody.MovePosition(transform.position += tDeplacement);
 
 
-            transform.forward = tDirection;
-        }
-     
-    }
-    private void Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            Vector3 tJump = Vector3.up * m_fVitesseDeSaut;
-            m_pRigidBody.AddForce(tJump, ForceMode.Impulse);
-        }
-    }
+	#endregion
+
+
+	private void Start()
+	{
+	}
+
+	private void Update()
+	{
+		MoveCharacter();
+		Jump();
+	}
+
+	private void MoveCharacter()
+	{
+		float fHorizontal = Input.GetAxis("Horizontal");
+		float fVertical = Input.GetAxis("Vertical");
+
+		Vector3 tDirection = new Vector3(fHorizontal, 0.0f, fVertical);
+
+		// Pression sur espace --> faire le reste de la fct° //
+		if (tDirection != Vector3.zero)
+		{
+			tDirection = CameraPersonnage.Instance.transform.TransformDirection(tDirection);
+			tDirection.y = 0.0f;
+
+			if (tDirection.sqrMagnitude != 0.0f)
+				tDirection.Normalize();
+			else
+				tDirection = transform.forward;
+
+
+			Vector3 tDeplacement = tDirection * (m_fVitesse * Time.deltaTime);
+			m_pRigidBody.MovePosition(transform.position += tDeplacement);
+
+
+			transform.forward = tDirection;
+		}
+	 
+	}
+	private void Jump()
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			Vector3 tJump = Vector3.up * m_fVitesseDeSaut;
+			m_pRigidBody.AddForce(tJump, ForceMode.Impulse);
+		}
+	}
 }
