830a8eda5c6b394381b5f1a9a59bad67857e294a
LeilaniBex <axelle.bini02@gmail.com>
2018-07-19 11:43:33 +0200

    Projectile et trampoline

--- Assets/Script/Armes/Projectile.cs
+++ Assets/Script/Armes/Projectile.cs
@@ -14,9 +14,17 @@ public class Projectile : MonoBehaviour
 
 	#region Variables (private)
 
-
+	private Vector3 m_tPositionDeDepart = Vector3.zero;
+	private Vector3 m_tDirectionDeDepart = Vector3.forward;
 
 	#endregion
+
+
+	private void Start()
+	{
+		m_tPositionDeDepart = transform.position;
+		m_tDirectionDeDepart = transform.forward;
+	}
 
 	private void Update()
 	{
@@ -25,10 +33,23 @@ public class Projectile : MonoBehaviour
 
 	private void DeplacerProjectile()
 	{
+
 		Vector3 tTrajectoire = new Vector3(0.0f, 0.0f, 0.0f);
 
-		tTrajectoire = transform.forward * (m_fVitesse * Time.deltaTime);
-
+		tTrajectoire = m_tDirectionDeDepart * (m_fVitesse * Time.deltaTime);
 		transform.position += tTrajectoire;
+
+		if ((transform.position - m_tPositionDeDepart).sqrMagnitude >= (m_fDistanceMax * m_fDistanceMax))
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void OnTriggerEnter(Collider pCollider)
+	{
+		if (pCollider.gameObject.layer != LayerMask.NameToLayer("Attaque") && pCollider.gameObject.layer != LayerMask.NameToLayer("Arme"))
+		{
+			Destroy(gameObject);
+		}
 	}
 }
