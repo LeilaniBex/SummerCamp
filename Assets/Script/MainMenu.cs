343f6364c432aeee9f1229ecfd6b6f816b6f9c34
LeilaniBex <axelle.bini02@gmail.com>
2018-07-19 15:18:19 +0200

    Creation du menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	#region Variables (public)
+
+	public string m_sNomDeLaSceneDeJeu = null;
+
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+	public void LancerJeu()
+	{
+		SceneManager.LoadScene(m_sNomDeLaSceneDeJeu);
+	}
+
+	public void QuitterJeu()
+	{
+		Application.Quit();
+	}
+
+}
